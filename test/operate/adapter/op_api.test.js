f71d5c93747f70b4e549bb5f0094fd807b29d568
libitx <libitx@protonmail.com>
2020-01-06 16:45:41 +0000

    Added adapter class and op_api adapter with tests.

---
+++ test/operate/adapter/op_api.test.js
@@ -0,0 +1,24 @@
+const { assert } = require('chai')
+const nock = require('nock')
+const OpApi = require('./../../../lib/operate/adapter/op_api')
+
+describe('OpApi.fetchOps() with array of references', () => {
+  before(() => {
+    nock('https://api.operatebsv.org/')
+      .get(/.*/)
+      .once()
+      .replyWithFile(200, 'test/mocks/api_fetch_ops.json', {
+        'Content-Type': 'application/json',
+      })
+  })
+
+  it('must return a list of functions', async () => {
+    const ops = await OpApi.fetchOps(['9ef5fd5c', '0ca59130'])
+    assert.isArray(ops)
+    ops.every(o => {
+      assert.equal(o.constructor.name, 'Op')
+    })
+    assert.include(ops.map(o => o.ref), '9ef5fd5c')
+    assert.include(ops.map(o => o.ref), '0ca59130')
+  })
+})
