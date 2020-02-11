0a9561032efdf27981c428d6a03ea2b09483ad63
libitx <libitx@protonmail.com>
2020-02-11 17:28:43 +0000

    Implements JSON extension.

---
+++ test/operate/vm/extension/json.test.js
@@ -0,0 +1,30 @@
+const { resolve } = require('path')
+const { assert } = require('chai')
+const VM = require(resolve('lib/operate/vm'))
+
+let vm;
+before(() => {
+  vm = new VM()
+})
+
+
+describe('JsonExt.encode()', () => {
+  it('must encode values as JSON strings', () => {
+    assert.equal(vm.eval("return json.encode('foo bar')"), "\"foo bar\"")
+    assert.equal(vm.eval("return json.encode(123)"), "123")
+    assert.equal(vm.eval("return json.encode({'foo', 'bar'})"), "[\"foo\",\"bar\"]")
+    assert.equal(vm.eval("return json.encode({foo = 'bar'})"), "{\"foo\":\"bar\"}")
+    assert.equal(vm.eval("return json.encode({foo = 'bar', 'baz'})"), "{\"1\":\"baz\",\"foo\":\"bar\"}")
+  })
+})
+
+
+describe('JsonExt.decode()', () => {
+  it('must decode JSON strings as Lua types', () => {
+    assert.isTrue(vm.eval("return json.decode('\"foo bar\"') == 'foo bar'"))
+    assert.isTrue(vm.eval("return json.decode(123) == 123"))
+    assert.isTrue(vm.eval("tab = json.decode('[\"foo\",\"bar\"]'); return tab[1] == 'foo' and tab[2] == 'bar'"))
+    assert.isTrue(vm.eval("tab = json.decode('{\"foo\":\"bar\"}'); return tab.foo == 'bar'"))
+    assert.isTrue(vm.eval("tab = json.decode('{\"1\":\"baz\",\"foo\":\"bar\"}'); return tab['1'] == 'baz'"))
+  })
+})
