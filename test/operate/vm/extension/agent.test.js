b6e0998254c61d232955d71e06c3241efd607f6f
libitx <libitx@protonmail.com>
2020-03-02 16:47:21 +0000

    Comments and code style.

--- test/operate/vm/extension/agent.test.js
+++ test/operate/vm/extension/agent.test.js
@@ -2,37 +2,34 @@ const { resolve } = require('path')
 const VM = require(resolve('lib/operate/vm'))
 const nock = require('nock')
 const { assert } = require('chai')
-const AgentExtension = require(resolve('lib/operate/vm/extension/agent'))
 const Agent = require(resolve('lib/operate/agent'))
 
-let aliases;
+let aliases
 before(() => {
   aliases = {
     '19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut': '6232de04',
     '1PuQa7K62MiKCtssSLKy1kh56WWU7MtUR5': '1fec30d4',
     '15PciHG22SNLQJXMoSUaWVi7WSqc7hCfva': 'a3a83843'
-  };
-});
+  }
+})
+
 
 describe('AgentExtension.loadTape, AgentExtension.runTape', () => {
   let vm;
   before(() => {
-    vm = new VM({
-      agent: new Agent(),
-      extensions: [AgentExtension]
-    });
+    vm = new VM({ agent: new Agent() })
     nock('https://bob.planaria.network/')
       .get(/.*/)
       .twice()
       .replyWithFile(200, 'test/mocks/agent_exec_get_tape.json', {
         'Content-Type': 'application/json'
-      });
+      })
     nock('https://api.operatebsv.org/')
       .get(/.*/)
       .twice()
       .replyWithFile(200, 'test/mocks/agent_exec_get_ops.json', {
         'Content-Type': 'application/json'
-      });
+      })
   })
 
   it('must load and run and return value of given tape', async () => {
@@ -49,6 +46,6 @@ describe('AgentExtension.loadTape, AgentExtension.runTape', () => {
       local tape = agent.load_tape('65aa086b2c54d5d792973db425b70712a708a115cd71fb67bd780e8ad9513ac9')
       return agent.run_tape(tape, {state = {'testing'}})`
     )
-    assert.include(res.get('numbers'), 'testing');
+    assert.include(res.get('numbers'), 'testing')
   })
-});
+})
--- test/operate/vm/extension/context.test.js
+++ test/operate/vm/extension/context.test.js
@@ -1,77 +1,89 @@
-const { resolve } = require("path");
-const VM = require(resolve("lib/operate/vm"));
-const { assert } = require("chai");
-const ContextExtension = require(resolve("lib/operate/vm/extension/context"));
-const { _toBPU } = require(resolve("lib/operate/adapter/bob"));
+const { resolve } = require('path')
+const VM = require(resolve('lib/operate/vm'))
+const { assert } = require('chai')
+const ContextExtension = require(resolve('lib/operate/vm/extension/context'))
+const { _toBPU } = require(resolve('lib/operate/adapter/bob'))
 
 let vm, vm2;
 before(() => {
-  vm = new VM();
-  vm2 = new VM();
-  const tx = require(resolve("test/mocks/bob_fetch_tx.json"));
-  const bpu = _toBPU(tx)[0];
-  vm.set("ctx.tx", bpu)
-    .set("ctx.tape_index", 0)
-    .set("ctx.cell_index", 2)
-    .set("ctx.data_index", 7);
-});
+  vm = new VM()
+  vm2 = new VM()
+  const tx = require(resolve('test/mocks/bob_fetch_tx.json'))
+  const bpu = _toBPU(tx)[0]
 
-describe("ContextExtension.txInput", () => {
-  it("with state must return the input by index", () => {
-    const res = vm.eval("return ctx.tx_input(0)");
-    assert.deepEqual(Object.keys(res), ["e", "i", "tape"]);
-    assert.deepEqual(Object.keys(res.e), ["a", "h", "i"]);
-  });
-  it("out of range must return nil", () => {
-    const res = vm.eval("return ctx.tx_input(1000)");
-    assert.isUndefined(res);
-  });
-  it("out of state must return nil", () => {
-    const res = vm2.eval("return ctx.tx_input(1)");
-    assert.isUndefined(res);
-  });
-});
+  vm.set('ctx.tx', bpu)
+    .set('ctx.tape_index', 0)
+    .set('ctx.cell_index', 2)
+    .set('ctx.data_index', 7)
+})
 
-describe("ContextExtension.txOutput", () => {
-  it("with state must return the output by index", () => {
-    const res = vm.eval("return ctx.tx_output(1)");
-    assert.deepEqual(Object.keys(res), ["e", "i", "tape"]);
-    assert.deepEqual(Object.keys(res.e), ["a", "i", "v"]);
-  });
-  it("out of range must return nil", () => {
-    const res = vm.eval("return ctx.tx_output(1000)");
-    assert.isUndefined(res);
-  });
-  it("without state must return nil", () => {
-    const res = vm2.eval("return ctx.tx_output(1)");
-    assert.isUndefined(res);
-  });
-});
 
-describe("ContextExtension.getTape", () => {
-  it("with state must return the current tape", () => {
-    const res = vm.eval("return ctx.get_tape()");
-    assert.lengthOf(res, 28);
-  });
-  it("without state must return nil", () => {
-    const res = vm2.eval("return ctx.get_tape()");
-    assert.isUndefined(res);
-  });
-});
+describe('ContextExtension.txInput', () => {
+  it('with state must return the input by index', () => {
+    const res = vm.eval('return ctx.tx_input(0)')
+    assert.deepEqual(Object.keys(res), ['e', 'i', 'tape'])
+    assert.deepEqual(Object.keys(res.e), ['a', 'h', 'i'])
+  })
 
-describe("ContextExtension.getCell", () => {
-  it("without index must return the current cell", () => {
-    const res = vm.eval("return ctx.get_cell()");
-    assert.equal(res[0].b, "1PuQa7K62MiKCtssSLKy1kh56WWU7MtUR5");
-    assert.equal(res.length, 16);
-  });
-  it("with index must return the requested cell", () => {
-    const res = vm.eval("return ctx.get_cell(1)");
-    assert.equal(res[0].b, "19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut");
-    assert.equal(res.length, 5);
-  });
-  it("without state must return nil", () => {
-    const res = vm2.eval("return ctx.get_cell()");
-    assert.isUndefined(res);
-  });
-});
+  it('out of range must return nil', () => {
+    const res = vm.eval('return ctx.tx_input(1000)')
+    assert.isUndefined(res)
+  })
+
+  it('out of state must return nil', () => {
+    const res = vm2.eval('return ctx.tx_input(1)')
+    assert.isUndefined(res)
+  })
+})
+
+
+describe('ContextExtension.txOutput', () => {
+  it('with state must return the output by index', () => {
+    const res = vm.eval('return ctx.tx_output(1)')
+    assert.deepEqual(Object.keys(res), ['e', 'i', 'tape'])
+    assert.deepEqual(Object.keys(res.e), ['a', 'i', 'v'])
+  })
+
+  it('out of range must return nil', () => {
+    const res = vm.eval('return ctx.tx_output(1000)')
+    assert.isUndefined(res)
+  })
+
+  it('without state must return nil', () => {
+    const res = vm2.eval('return ctx.tx_output(1)')
+    assert.isUndefined(res)
+  })
+})
+
+
+describe('ContextExtension.getTape', () => {
+  it('with state must return the current tape', () => {
+    const res = vm.eval('return ctx.get_tape()')
+    assert.lengthOf(res, 28)
+  })
+
+  it('without state must return nil', () => {
+    const res = vm2.eval('return ctx.get_tape()')
+    assert.isUndefined(res)
+  })
+})
+
+
+describe('ContextExtension.getCell', () => {
+  it('without index must return the current cell', () => {
+    const res = vm.eval('return ctx.get_cell()')
+    assert.equal(res[0].b, '1PuQa7K62MiKCtssSLKy1kh56WWU7MtUR5')
+    assert.equal(res.length, 16)
+  })
+
+  it('with index must return the requested cell', () => {
+    const res = vm.eval('return ctx.get_cell(1)')
+    assert.equal(res[0].b, '19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut')
+    assert.equal(res.length, 5)
+  })
+
+  it('without state must return nil', () => {
+    const res = vm2.eval('return ctx.get_cell()')
+    assert.isUndefined(res)
+  })
+})
