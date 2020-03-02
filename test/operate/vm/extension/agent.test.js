afc67fe7de87dee266b82d5d395396846a5c7a7c
libitx <libitx@protonmail.com>
2020-03-02 16:01:33 +0000

    Use bops for all text encodings. Remove utils module. Explicit function for vm setAsyncFunction. Some style changes.

--- test/operate/vm/extension/agent.test.js
+++ test/operate/vm/extension/agent.test.js
@@ -1,51 +1,54 @@
-const { resolve } = require("path");
-const VM = require(resolve("lib/operate/vm"));
-const nock = require("nock");
-const { assert } = require("chai");
-const AgentExtension = require(resolve("lib/operate/vm/extension/agent"));
-const Agent = require(resolve("lib/operate/agent"));
+const { resolve } = require('path')
+const VM = require(resolve('lib/operate/vm'))
+const nock = require('nock')
+const { assert } = require('chai')
+const AgentExtension = require(resolve('lib/operate/vm/extension/agent'))
+const Agent = require(resolve('lib/operate/agent'))
 
 let aliases;
 before(() => {
   aliases = {
-    "19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut": "6232de04",
-    "1PuQa7K62MiKCtssSLKy1kh56WWU7MtUR5": "1fec30d4",
-    "15PciHG22SNLQJXMoSUaWVi7WSqc7hCfva": "a3a83843"
+    '19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut': '6232de04',
+    '1PuQa7K62MiKCtssSLKy1kh56WWU7MtUR5': '1fec30d4',
+    '15PciHG22SNLQJXMoSUaWVi7WSqc7hCfva': 'a3a83843'
   };
 });
 
-describe("AgentExtension.loadTape, AgentExtension.runTape", () => {
+describe('AgentExtension.loadTape, AgentExtension.runTape', () => {
   let vm;
   before(() => {
     vm = new VM({
       agent: new Agent(),
       extensions: [AgentExtension]
     });
-    nock("https://bob.planaria.network/")
+    nock('https://bob.planaria.network/')
       .get(/.*/)
       .twice()
-      .replyWithFile(200, "test/mocks/agent_exec_get_tape.json", {
-        "Content-Type": "application/json"
+      .replyWithFile(200, 'test/mocks/agent_exec_get_tape.json', {
+        'Content-Type': 'application/json'
       });
-    nock("https://api.operatebsv.org/")
+    nock('https://api.operatebsv.org/')
       .get(/.*/)
       .twice()
-      .replyWithFile(200, "test/mocks/agent_exec_get_ops.json", {
-        "Content-Type": "application/json"
+      .replyWithFile(200, 'test/mocks/agent_exec_get_ops.json', {
+        'Content-Type': 'application/json'
       });
-  }),
-    it("must load and run and return value of given tape", () => {
-      const res = vm.eval(
-        `local tape = agent.load_tape('65aa086b2c54d5d792973db425b70712a708a115cd71fb67bd780e8ad9513ac9')
-        return agent.run_tape(tape)`
-      );
-      assert.equal(res, ["name", "numbers"]);
-    }),
-    it("must build on the given state", () => {
-      const res = vm.eval(
-        `local tape = agent.load_tape('65aa086b2c54d5d792973db425b70712a708a115cd71fb67bd780e8ad9513ac9')
-        return agent.run_tape(tape, {state = {'testing'}})`
-      );
-      assert.equal(res.numbers[0], "testing");
-    });
+  })
+
+  it('must load and run and return value of given tape', async () => {
+    const res = await vm.evalAsync(`
+      local tape = agent.load_tape('65aa086b2c54d5d792973db425b70712a708a115cd71fb67bd780e8ad9513ac9')
+      return agent.run_tape(tape)`
+    )
+    assert.include([...res.keys()], 'name')
+    assert.include([...res.keys()], 'numbers')
+  })
+
+  it('must build on the given state', async () => {
+    const res = await vm.evalAsync(`
+      local tape = agent.load_tape('65aa086b2c54d5d792973db425b70712a708a115cd71fb67bd780e8ad9513ac9')
+      return agent.run_tape(tape, {state = {'testing'}})`
+    )
+    assert.include(res.get('numbers'), 'testing');
+  })
 });
--- test/operate/vm/extension/base.test.js
+++ test/operate/vm/extension/base.test.js
@@ -1,36 +1,43 @@
-const { resolve } = require("path");
-const VM = require(resolve("lib/operate/vm"));
-const { assert } = require("chai");
-const BaseExtension = require(resolve("lib/operate/vm/extension/base"));
+const { resolve } = require('path')
+const VM = require(resolve('lib/operate/vm'))
+const { assert } = require('chai')
+const BaseExtension = require(resolve('lib/operate/vm/extension/base'))
 
-describe("base.encode16, base.decode16", () => {
-  let vm;
+
+describe('base.encode16, base.decode16', () => {
+  let vm
   before(() => {
     vm = new VM({
       extensions: [BaseExtension]
-    });
-  });
-  it("must encode binary string as hex string", () => {
-    assert.equal(vm.eval(`return base.encode16('foo bar')`), "666f6f20626172");
-  });
-  it("must encode hex string as binary string", () => {
-    assert.equal(vm.eval(`return base.decode16('666f6f20626172')`), "foo bar");
-    assert.equal(vm.eval(`return base.decode16('666F6F20626172')`), "foo bar");
-    assert.equal(vm.eval(`return base.decode16('666F6f20626172')`), "foo bar");
-  });
-});
+    })
+  })
 
-describe("base.encode64, base.decode64", () => {
-  let vm;
+  it('must encode binary string as hex string', () => {
+    assert.equal(vm.eval(`return base.encode16('foo bar')`), '666f6f20626172')
+  })
+
+  it('must encode hex string as binary string', () => {
+    assert.equal(vm.eval(`return base.decode16('666f6f20626172')`), 'foo bar')
+    // With mixed casings
+    assert.equal(vm.eval(`return base.decode16('666F6F20626172')`), 'foo bar')
+    assert.equal(vm.eval(`return base.decode16('666F6f20626172')`), 'foo bar')
+  })
+})
+
+
+describe('base.encode64, base.decode64', () => {
+  let vm
   before(() => {
     vm = new VM({
       extensions: [BaseExtension]
-    });
-  });
-  it("must encode binary string as hex string", () => {
-    assert.equal(vm.eval(`return base.encode64('foo bar')`), "Zm9vIGJhcg==");
-  });
-  it("must encode hex string as binary string", () => {
-    assert.equal(vm.eval(`return base.decode64('Zm9vIGJhcg==')`), "foo bar");
-  });
-});
+    })
+  })
+
+  it('must encode binary string as hex string', () => {
+    assert.equal(vm.eval(`return base.encode64('foo bar')`), 'Zm9vIGJhcg==')
+  })
+
+  it('must encode hex string as binary string', () => {
+    assert.equal(vm.eval(`return base.decode64('Zm9vIGJhcg==')`), 'foo bar')
+  })
+})
