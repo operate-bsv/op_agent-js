71c4cbbc30d8eef5bef09de5737dc5f0a8ed9c8c
libitx <libitx@protonmail.com>
2020-03-11 11:54:18 +0000

    Better docs.

---
+++ jsdoc.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  source: {
+    include: [
+      'README.md',
+      'lib/index.js',
+      'lib/operate/'
+    ]
+  },
+  opts: {
+    recurse: true,
+    destination: 'docs',
+    template: 'node_modules/better-docs'
+  },
+  plugins: [
+    'plugins/markdown',
+    'node_modules/better-docs/category'
+  ],
+  tags: {
+    allowUnknownTags: ['category']
+  },
+  templates: {
+    default: {
+      includeDate: false
+    },
+    'better-docs': {
+      name: 'Operate | Agent (js)',
+      navigation: [
+        { label: 'Homepage', href: 'https://www.operatebsv.org' },
+        { label: 'Github', href: 'https://github.com/operate-bsv/op_agent-js' }
+      ]
+    }
+  }
+}
